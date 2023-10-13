fbc969f3871b324762002373467175f57883d1c0
Alek <aleksander.dul.77@gmail.com>
2023-10-13 08:39:27 +0200

    init backend commit

---
+++ backend/src/main/java/com/trackmycar/trackmycarbackend/TrackMyCarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trackmycar.trackmycarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyCarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyCarBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/trackmycar/trackmycarbackend/TrackmycarbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmycar.trackmycarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmycarbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
