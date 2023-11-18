72d1ca502aceab7f62c560539890306f2d393817
Alek <aleksander.dul.77@gmail.com>
2023-11-18 11:02:11 +0100

    added get vehicle (by id) last position endpoint

--- backend/src/main/java/com/trackmycar/trackmycarbackend/controller/VehicleController.java
+++ backend/src/main/java/com/trackmycar/trackmycarbackend/controller/VehicleController.java
@@ -2,13 +2,13 @@ package com.trackmycar.trackmycarbackend.controller;
 
 import com.trackmycar.trackmycarbackend.dto.ApiExceptionDto;
 import com.trackmycar.trackmycarbackend.dto.VehicleDto;
+import com.trackmycar.trackmycarbackend.dto.VehicleLastPositionDto;
 import com.trackmycar.trackmycarbackend.model.AppUser;
+import com.trackmycar.trackmycarbackend.model.Position;
 import com.trackmycar.trackmycarbackend.model.Vehicle;
 import com.trackmycar.trackmycarbackend.exception.*;
-import com.trackmycar.trackmycarbackend.service.AuthorizationService;
-import com.trackmycar.trackmycarbackend.service.TokenService;
-import com.trackmycar.trackmycarbackend.service.UserService;
-import com.trackmycar.trackmycarbackend.service.VehicleService;
+import com.trackmycar.trackmycarbackend.model.VehicleTrackerAssignment;
+import com.trackmycar.trackmycarbackend.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -25,16 +25,19 @@ public class VehicleController {
     private final TokenService tokenService;
     private final UserService userService;
     private final VehicleService vehicleService;
+    private final VehicleTrackerAssignmentService vehicleTrackerAssignmentService;
     private final AuthorizationService authorizationService;
 
     @Autowired
     public VehicleController(TokenService tokenService,
                              UserService userService,
                              VehicleService vehicleService,
+                             VehicleTrackerAssignmentService vehicleTrackerAssignmentService,
                              AuthorizationService authorizationService) {
         this.tokenService = tokenService;
         this.userService = userService;
         this.vehicleService = vehicleService;
+        this.vehicleTrackerAssignmentService = vehicleTrackerAssignmentService;
         this.authorizationService = authorizationService;
     }
 
@@ -60,6 +63,26 @@ public class VehicleController {
         authorizationService.checkResourceAccessAuthorization(vehicle, user, Vehicle::getOwner);
 
         return new VehicleDto(vehicle);
+    }
+
+    @GetMapping(path="/{vehicleId}/last-position")
+    public VehicleLastPositionDto getVehicleLastPosition(@RequestHeader(HttpHeaders.AUTHORIZATION) String token,
+                                                         @PathVariable("vehicleId") Integer vehicleId) {
+        String username = tokenService.getUsernameFromToken(token);
+        AppUser user = userService.getUserByUsername(username);
+        VehicleTrackerAssignment assignment =
+                vehicleTrackerAssignmentService.getActiveAssignmentByVehicleId(vehicleId);
+
+        authorizationService.checkResourceAccessAuthorization(
+                assignment, user, VehicleTrackerAssignment::getOwner);
+
+        return new VehicleLastPositionDto(
+                user.getUserId(),
+                vehicleId,
+                assignment.getLastPosTimestamp(),
+                assignment.getLastPosLatitude(),
+                assignment.getLastPosLongitude()
+        );
     }
 
     @PostMapping
