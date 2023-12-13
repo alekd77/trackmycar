124798b08effccec80cd3a8de03000389e7ea9e2
Alek <aleksander.dul.77@gmail.com>
2023-12-14 00:29:52 +0100

    added exception handler case to global handler

--- backend/src/main/java/com/trackmycar/trackmycarbackend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/trackmycar/trackmycarbackend/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.trackmycar.trackmycarbackend.exception;
 
 import com.trackmycar.trackmycarbackend.dto.ApiExceptionDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,5 +27,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({InvalidInputException.class})
     public ResponseEntity<ApiExceptionDto> handleInvalidInput(InvalidInputException ex) {
         return new ResponseEntity<>(new ApiExceptionDto(ex), ex.getStatus());
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<ApiExceptionDto> handleAccessDenied(AccessDeniedException ex) {
+        // Customize the response as needed
+        return new ResponseEntity<>(new ApiExceptionDto(
+                new ApiException(
+                        "Access denied",
+                        HttpStatus.FORBIDDEN,
+                        ex.getMessage()
+                )), HttpStatus.FORBIDDEN);
     }
 }
