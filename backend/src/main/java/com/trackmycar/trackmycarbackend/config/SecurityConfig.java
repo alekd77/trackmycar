1fcadf6d46d14ce684b13e8c7bdd93706d33dfab
Alek <aleksander.dul.77@gmail.com>
2024-03-29 19:46:39 +0100

    added github pages origin to cors config

--- backend/src/main/java/com/trackmycar/trackmycarbackend/config/SecurityConfig.java
+++ backend/src/main/java/com/trackmycar/trackmycarbackend/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "https://alekd77.github.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
